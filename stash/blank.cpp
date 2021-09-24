5dab93bd89bee3046c158158d9a320ef4da612ba
Dmitry Ulyumdzhiev <ulyumjiev.dmitry@yandex.ru>
2021-09-24 20:23:05 +0300

    new pragmas

--- stash/blank.cpp
+++ stash/blank.cpp
@@ -1,6 +1,6 @@
 #pragma GCC target( \
     "sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
-#pragma GCC optimize("O3,no-stack-protector")
+#pragma GCC optimize("-O3,-funroll-loops,-march=native,-mtune=native")
 #include <bits/stdc++.h>
 
 using namespace std;

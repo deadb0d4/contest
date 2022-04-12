405a00ed48d21169a8ed23bd7c00879ac9eb62f5
Dmitry Ulyumdzhiev <dulumzhiev@gmail.com>
2022-04-12 21:04:14 +0300

    use macro for local run

--- stash/blank.cpp
+++ stash/blank.cpp
@@ -1,6 +1,9 @@
+#ifndef EBUG
 #pragma GCC target( \
-    "sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,lzcnt,bmi,bmi2")
+  "sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,lzcnt,bmi,bmi2")
 #pragma GCC optimize("-O3,-funroll-loops,-march=native,-mtune=native")
+#endif
+
 #include <bits/stdc++.h>
 
 using namespace std;
